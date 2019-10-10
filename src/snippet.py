e374c29fa290b25f166b11a68d2ce5e64287d8b1
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-10-10 18:48:44 +0200

    Finished point 4

--- src/snippet.py
+++ src/snippet.py
@@ -30,18 +30,7 @@ def create_dataset(w_star, x_range, sample_size, sigma, seed=None):
     return X, y
 
 
-def main():
-    n_iterations = 1100
-    sample_size = 100
-    n_dimensions = 4
-    sigma = 0.5
-
-    # Naming constants/variables to facilitate inspection
-    learning_rate = tf.constant(1e-2, dtype=tf.float32, name='learning_rate')
-
-    w_star = np.array([-8, -4, 2, 1], dtype=np.float32)
-    x_range = [-3, 2]
-
+def main(n_iterations, sample_size, n_dimensions, sigma, learning_rate, w_star, x_range):
     # Placeholder for the data matrix, where each observation is a row
     X = tf.placeholder(tf.float32, shape=(None, n_dimensions), name='X')  # Placeholder for the targets
     y = tf.placeholder(tf.float32, shape=(None,), name='y')
@@ -61,12 +50,10 @@ def main():
 
     initializer = tf.global_variables_initializer()
 
-    X_train, y_train = create_dataset(w_star, x_range, sample_size, sigma, 0)
-    X_val, y_val = create_dataset(w_star, x_range, sample_size, sigma, 1)
+    X_train, y_train = create_dataset(w_star, x_range, sample_size[0], sigma, 0)
+    X_val, y_val = create_dataset(w_star, x_range, sample_size[1], sigma, 1)
 
     # Plot dataset
-    #fig = plt.figure()
-
     plt.plot(X_train[:, 1], y_train, '.')
     plt.plot(X_val[:, 1], y_val, '.')
 
@@ -83,23 +70,65 @@ def main():
 
     session.run(initializer)
 
+    validation_loss = 0
     for t in range(1, n_iterations + 1):
-        s, l, _ = session.run([summaries, loss, train], feed_dict={X: X_train, y: y_train})
-        print('Iteration {0}. Loss: {1}.'.format(t, l))
+        s, train_loss, _ = session.run([summaries, loss, train], feed_dict={X: X_train, y: y_train})
+        print('Iteration {0}. Loss: {1}.'.format(t, train_loss))
 
         # Stores the summaries for iteration t
         writer_train.add_summary(s, t)
 
-        s, l = session.run([summaries, loss], feed_dict={X: X_val, y: y_val})
+        s, validation_loss = session.run([summaries, loss], feed_dict={X: X_val, y: y_val})
         writer_validation.add_summary(s, t)
 
-    print('Validation loss: {0}.'.format(l))
+    print('Validation loss: {0}.'.format(validation_loss))
     print(session.run(w).reshape(-1))
 
     writer_train.close()
     writer_validation.close()
+
+    # Plot polynom
+    w_hat = session.run(w)
+    x = np.linspace(x_range[0], x_range[1])
+
+    plt.plot(x, extract_y(x, w_star))
+    plt.plot(x, extract_y(x, w_hat))
+
+    plt.show()
+
     session.close()
 
 
+def extract_feature_map(x, degree=3):
+    f = np.zeros([x.shape[0], degree + 1])
+
+    for d in range(degree + 1):
+        f[:, d] = x ** d
+    return f
+
+
+def extract_y(x, coefficients):
+    """ Returns a polynomial for ``x`` values for the ``coeffs`` provided.
+
+    The coefficients must be in ascending order (``x**0`` to ``x**o``).
+    """
+    y = np.matmul(extract_feature_map(x, 3), coefficients)
+    return y
+
+
+################################################################################
+
+
+n_iterations = 1100
+sample_size = [100, 100]  # [train, validatioon]
+n_dimensions = 4
+sigma = 0.5
+
+# Naming constants/variables to facilitate inspection
+learning_rate = tf.constant(1e-2, dtype=tf.float32, name='learning_rate')
+
+w_star = np.array([-8, -4, 2, 1], dtype=np.float32)
+x_range = [-3, 2]
+
 # run the script
-main()
+main(n_iterations, sample_size, n_dimensions, sigma, learning_rate, w_star, x_range)
